97b764151256f6a121888af22e5e1faa4b9e74fb
Carlos Barboza <Carlos Barboza@001-ADC-GREIVIN>
2016-11-15 15:47:37 -0600

    Agregar clase de la tabla Rol de Persona por Vehiculo

--- Cosevi.SIBOAC/Models/RolDePersonaPorVehiculo.cs
+++ Cosevi.SIBOAC/Models/RolDePersonaPorVehiculo.cs
@@ -11,13 +11,32 @@ namespace Cosevi.SIBOAC.Models
 {
     using System;
     using System.Collections.Generic;
-    
+    using System.ComponentModel;
+    using System.ComponentModel.DataAnnotations;
+
     public partial class RolDePersonaPorVehiculo
     {
+        [DisplayName("Codigo")]
+        [StringLength(2, ErrorMessage ="El codigo no debe ser mayor a 2 caracteres")]
         public string Id { get; set; }
+
+        [DisplayName("Activar Vehiculo")]
         public bool ActivarVehiculo { get; set; }
+
+        [DisplayName("Estado")]
+        [StringLength(1, ErrorMessage = "El estado no debe ser mayor a 1 caracter")]
         public string Estado { get; set; }
+
+        [DisplayName("Fecha de Inicio")]
+        [Required(ErrorMessage = "La fecha de inicio es obligatoria")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}",ApplyFormatInEditMode = true)]
         public Nullable<System.DateTime> FechaDeInicio { get; set; }
+
+        [DisplayName("Fecha de Fin")]
+        [Required(ErrorMessage = "La fecha de fin es obligatoria")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public Nullable<System.DateTime> FechaDeFin { get; set; }
     }
 }
