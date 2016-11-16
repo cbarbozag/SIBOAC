6467e9cb222e6b429663e8efc4e4fbdc30949acc
nzamora <nzamora@ADC-LT-DESA01>
2016-11-16 14:42:04 -0600

    Add Controladores Direccion, Distrito, Examen, Inspector(Se genera pero al consultar genera error en el navegador)

---
+++ Cosevi.SIBOAC/Controllers/DireccionsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Cosevi.SIBOAC.Models;
+
+namespace Cosevi.SIBOAC.Controllers
+{
+    public class DireccionsController : Controller
+    {
+        private PC_HH_AndroidEntities db = new PC_HH_AndroidEntities();
+
+        // GET: Direccions
+        public ActionResult Index()
+        {
+            return View(db.DIRECCION.ToList());
+        }
+
+        // GET: Direccions/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Direccion direccion = db.DIRECCION.Find(id);
+            if (direccion == null)
+            {
+                return HttpNotFound();
+            }
+            return View(direccion);
+        }
+
+        // GET: Direccions/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Direccions/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Descripcion,Estado,FechaDeInicio,FechaDeFin")] Direccion direccion)
+        {
+            if (ModelState.IsValid)
+            {
+                db.DIRECCION.Add(direccion);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(direccion);
+        }
+
+        // GET: Direccions/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Direccion direccion = db.DIRECCION.Find(id);
+            if (direccion == null)
+            {
+                return HttpNotFound();
+            }
+            return View(direccion);
+        }
+
+        // POST: Direccions/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Descripcion,Estado,FechaDeInicio,FechaDeFin")] Direccion direccion)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(direccion).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(direccion);
+        }
+
+        // GET: Direccions/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Direccion direccion = db.DIRECCION.Find(id);
+            if (direccion == null)
+            {
+                return HttpNotFound();
+            }
+            return View(direccion);
+        }
+
+        // POST: Direccions/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Direccion direccion = db.DIRECCION.Find(id);
+            db.DIRECCION.Remove(direccion);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Cosevi.SIBOAC/Controllers/DistritoesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Cosevi.SIBOAC.Models;
+
+namespace Cosevi.SIBOAC.Controllers
+{
+    public class DistritoesController : Controller
+    {
+        private PC_HH_AndroidEntities db = new PC_HH_AndroidEntities();
+
+        // GET: Distritoes
+        public ActionResult Index()
+        {
+            return View(db.DISTRITO.ToList());
+        }
+
+        // GET: Distritoes/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Distrito distrito = db.DISTRITO.Find(id);
+            if (distrito == null)
+            {
+                return HttpNotFound();
+            }
+            return View(distrito);
+        }
+
+        // GET: Distritoes/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Distritoes/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Descripcion,Estado,FechaDeInicio,FechaDeFin")] Distrito distrito)
+        {
+            if (ModelState.IsValid)
+            {
+                db.DISTRITO.Add(distrito);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(distrito);
+        }
+
+        // GET: Distritoes/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Distrito distrito = db.DISTRITO.Find(id);
+            if (distrito == null)
+            {
+                return HttpNotFound();
+            }
+            return View(distrito);
+        }
+
+        // POST: Distritoes/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Descripcion,Estado,FechaDeInicio,FechaDeFin")] Distrito distrito)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(distrito).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(distrito);
+        }
+
+        // GET: Distritoes/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Distrito distrito = db.DISTRITO.Find(id);
+            if (distrito == null)
+            {
+                return HttpNotFound();
+            }
+            return View(distrito);
+        }
+
+        // POST: Distritoes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Distrito distrito = db.DISTRITO.Find(id);
+            db.DISTRITO.Remove(distrito);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
