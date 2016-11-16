228e4ef4a7888ce1c797cfac978a523b0067cdb7
nzamora <nzamora@ADC-LT-DESA01>
2016-11-16 10:02:07 -0600

    Se agregaron los controladores de Plantillas,Provincias, Señalamiento,Sentidos,Sexo,Tipo de licencia

---
+++ Cosevi.SIBOAC/Controllers/SexoesController.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Cosevi.SIBOAC.Models
+{
+    public class SexoesController : Controller
+    {
+        private PC_HH_AndroidEntities db = new PC_HH_AndroidEntities();
+
+        // GET: Sexoes
+        public ActionResult Index()
+        {
+            return View(db.SEXO.ToList());
+        }
+
+        // GET: Sexoes/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Sexo sexo = db.SEXO.Find(id);
+            if (sexo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sexo);
+        }
+
+        // GET: Sexoes/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Sexoes/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Descripcion,Estado,FechaDeInicio,FechaDeFin")] Sexo sexo)
+        {
+            if (ModelState.IsValid)
+            {
+                db.SEXO.Add(sexo);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(sexo);
+        }
+
+        // GET: Sexoes/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Sexo sexo = db.SEXO.Find(id);
+            if (sexo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sexo);
+        }
+
+        // POST: Sexoes/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Descripcion,Estado,FechaDeInicio,FechaDeFin")] Sexo sexo)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(sexo).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(sexo);
+        }
+
+        // GET: Sexoes/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Sexo sexo = db.SEXO.Find(id);
+            if (sexo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sexo);
+        }
+
+        // POST: Sexoes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            Sexo sexo = db.SEXO.Find(id);
+            db.SEXO.Remove(sexo);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
