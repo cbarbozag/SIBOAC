18ced707af40af0cde7df299a54cf0efff920a89
Carlos Barboza <Carlos Barboza@001-ADC-GREIVIN>
2016-11-16 13:42:11 -0600

    Agregar clase de la tabla delegación

--- Cosevi.SIBOAC/Controllers/DelegacionsController.cs
+++ Cosevi.SIBOAC/Controllers/DelegacionsController.cs
@@ -110,7 +110,7 @@ namespace Cosevi.SIBOAC.Controllers
         public ActionResult DeleteConfirmed(string id)
         {
             Delegacion delegacion = db.DELEGACION.Find(id);
-            db.DELEGACION.Remove(delegacion);
+            delegacion.Estado = "I";
             db.SaveChanges();
             return RedirectToAction("Index");
         }
