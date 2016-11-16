fbaf7551ff54f7a879f322249c9a8633f4f309d3
Carlos Barboza <Carlos Barboza@001-ADC-GREIVIN>
2016-11-16 15:44:17 -0600

    Agregar clase de la Tabla Dirección

--- Cosevi.SIBOAC/Controllers/DireccionsController.cs
+++ Cosevi.SIBOAC/Controllers/DireccionsController.cs
@@ -110,7 +110,7 @@ namespace Cosevi.SIBOAC.Controllers
         public ActionResult DeleteConfirmed(int id)
         {
             Direccion direccion = db.DIRECCION.Find(id);
-            db.DIRECCION.Remove(direccion);
+            direccion.Estado = "I";
             db.SaveChanges();
             return RedirectToAction("Index");
         }
