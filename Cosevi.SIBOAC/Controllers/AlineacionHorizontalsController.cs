d3b6762b4798a8a2a04ae96d19ebb9a8d92ddfe5
nzamora <nzamora@ADC-LT-DESA01>
2016-11-16 13:57:20 -0600

    add Controladores Alineacion:Vertical y Horizontal

---
+++ Cosevi.SIBOAC/Controllers/AlineacionHorizontalsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Cosevi.SIBOAC.Models;
+
+namespace Cosevi.SIBOAC.Controllers
+{
+    public class AlineacionHorizontalsController : Controller
+    {
+        private PC_HH_AndroidEntities db = new PC_HH_AndroidEntities();
+
+        // GET: AlineacionHorizontals
+        public ActionResult Index()
+        {
+            return View(db.ALINHORI.ToList());
+        }
+
+        // GET: AlineacionHorizontals/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            AlineacionHorizontal alineacionHorizontal = db.ALINHORI.Find(id);
+            if (alineacionHorizontal == null)
+            {
+                return HttpNotFound();
+            }
+            return View(alineacionHorizontal);
+        }
+
+        // GET: AlineacionHorizontals/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: AlineacionHorizontals/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Descripcion,Estado,FechaDeInicio,FechaDeFin")] AlineacionHorizontal alineacionHorizontal)
+        {
+            if (ModelState.IsValid)
+            {
+                db.ALINHORI.Add(alineacionHorizontal);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(alineacionHorizontal);
+        }
+
+        // GET: AlineacionHorizontals/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            AlineacionHorizontal alineacionHorizontal = db.ALINHORI.Find(id);
+            if (alineacionHorizontal == null)
+            {
+                return HttpNotFound();
+            }
+            return View(alineacionHorizontal);
+        }
+
+        // POST: AlineacionHorizontals/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Descripcion,Estado,FechaDeInicio,FechaDeFin")] AlineacionHorizontal alineacionHorizontal)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(alineacionHorizontal).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(alineacionHorizontal);
+        }
+
+        // GET: AlineacionHorizontals/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            AlineacionHorizontal alineacionHorizontal = db.ALINHORI.Find(id);
+            if (alineacionHorizontal == null)
+            {
+                return HttpNotFound();
+            }
+            return View(alineacionHorizontal);
+        }
+
+        // POST: AlineacionHorizontals/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            AlineacionHorizontal alineacionHorizontal = db.ALINHORI.Find(id);
+            db.ALINHORI.Remove(alineacionHorizontal);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Cosevi.SIBOAC/Controllers/AlineacionVerticalsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Cosevi.SIBOAC.Models;
+
+namespace Cosevi.SIBOAC.Controllers
+{
+    public class AlineacionVerticalsController : Controller
+    {
+        private PC_HH_AndroidEntities db = new PC_HH_AndroidEntities();
+
+        // GET: AlineacionVerticals
+        public ActionResult Index()
+        {
+            return View(db.ALINVERT.ToList());
+        }
+
+        // GET: AlineacionVerticals/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            AlineacionVertical alineacionVertical = db.ALINVERT.Find(id);
+            if (alineacionVertical == null)
+            {
+                return HttpNotFound();
+            }
+            return View(alineacionVertical);
+        }
+
+        // GET: AlineacionVerticals/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: AlineacionVerticals/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Descripcion,Estado,FechaDeInicio,FechaDeFin")] AlineacionVertical alineacionVertical)
+        {
+            if (ModelState.IsValid)
+            {
+                db.ALINVERT.Add(alineacionVertical);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(alineacionVertical);
+        }
+
+        // GET: AlineacionVerticals/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            AlineacionVertical alineacionVertical = db.ALINVERT.Find(id);
+            if (alineacionVertical == null)
+            {
+                return HttpNotFound();
+            }
+            return View(alineacionVertical);
+        }
+
+        // POST: AlineacionVerticals/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Descripcion,Estado,FechaDeInicio,FechaDeFin")] AlineacionVertical alineacionVertical)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(alineacionVertical).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(alineacionVertical);
+        }
+
+        // GET: AlineacionVerticals/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            AlineacionVertical alineacionVertical = db.ALINVERT.Find(id);
+            if (alineacionVertical == null)
+            {
+                return HttpNotFound();
+            }
+            return View(alineacionVertical);
+        }
+
+        // POST: AlineacionVerticals/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            AlineacionVertical alineacionVertical = db.ALINVERT.Find(id);
+            db.ALINVERT.Remove(alineacionVertical);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
