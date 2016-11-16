7537c41a1816a45cf9ac3ed856a81a33275dc62b
nzamora <nzamora@ADC-LT-DESA01>
2016-11-16 17:22:57 -0600

    Actualizar depósito de placas

--- Cosevi.SIBOAC/Models/DepositoDePlaca.cs
+++ Cosevi.SIBOAC/Models/DepositoDePlaca.cs
@@ -11,13 +11,36 @@ namespace Cosevi.SIBOAC.Models
 {
     using System;
     using System.Collections.Generic;
-    
+    using System.ComponentModel;
+    using System.ComponentModel.DataAnnotations;
+
     public partial class DepositoDePlaca
     {
+        [DisplayName("Código Oficina")]
+        [Required(ErrorMessage = "El código es obligatoria")]
+        [StringLength(2, ErrorMessage = "El código no debe ser mayor a 2 caracteres")]
         public string Id { get; set; }
+
+        [DisplayName("Oficina")]
+        [Required(ErrorMessage = "La descripción de la oficina es obligatoria")]
+        [StringLength(50, ErrorMessage = "La descripción de oficina no debe ser mayor a 50 caracteres")]
         public string Descripcion { get; set; }
+
+        [DisplayName("Estado")]
+        [Required(ErrorMessage = "El estado es obligatorio")]
+        [StringLength(1, ErrorMessage = "El estado no debe ser mayor a 1 caracter")]
         public string Estado { get; set; }
+
+        [DisplayName("Fecha de Inicio")]
+        [Required(ErrorMessage = "La fecha de inicio es obligatoria")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public Nullable<System.DateTime> FechaDeInicio { get; set; }
+
+        [DisplayName("Fecha de Fin")]
+        [Required(ErrorMessage = "La fecha de fin es obligatoria")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public Nullable<System.DateTime> FechaDeFin { get; set; }
     }
 }
