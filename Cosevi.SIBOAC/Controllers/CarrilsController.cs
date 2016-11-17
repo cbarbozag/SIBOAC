066d795a8d98b0b28ee0d170e3a0d38ff69445cd
soporte <soporte@SERVIDOR>
2016-11-17 09:22:55 -0600

    Actualización de vistas y controlador de tabla Carril.

--- Cosevi.SIBOAC/Controllers/CarrilsController.cs
+++ Cosevi.SIBOAC/Controllers/CarrilsController.cs
@@ -110,7 +110,7 @@ namespace Cosevi.SIBOAC.Controllers
         public ActionResult DeleteConfirmed(string id)
         {
             Carril carril = db.CARRIL.Find(id);
-            db.CARRIL.Remove(carril);
+            carril.Estado = "I";
             db.SaveChanges();
             return RedirectToAction("Index");
         }
