d06e28a0d296e2ae0323124fa7e894e50752f349
Eduardo Oviedo Blanco <eduardo.oviedo@gmail.com>
2016-11-17 09:32:23 -0600

    dashboard

---
+++ Cosevi.SIBOAC/Scripts/app.js
@@ -0,0 +1,17 @@
+﻿// Side menu slide event
+$(document).ready(function () {
+    $('[data-toggle=offcanvas]').click(function () {
+        $('.row-offcanvas').toggleClass('active');
+    });
+
+    var loc = window.location.pathname;
+    $('.nav-stacked').find('a').each(function () {
+        $(this).parent().toggleClass('active', $(this).attr('href') == loc);
+    });
+});
+
+// Side menu click event
+$(".nav a").on("click", function () {
+    $(".nav").find(".active").removeClass("active");
+    $(this).parent().addClass("active");
+});
