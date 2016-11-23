f1240fc6cee13ca834e823955ccea35f6b5933e4
Carlos Barboza <Carlos Barboza@001-ADC-GREIVIN>
2016-11-23 16:44:32 -0600

    Agregar modificaciones de los controllers y alertas de id duplicado

--- Cosevi.SIBOAC/Controllers/CondicionDeLaCalzadasController.cs
+++ Cosevi.SIBOAC/Controllers/CondicionDeLaCalzadasController.cs
@@ -17,7 +17,20 @@ namespace Cosevi.SIBOAC.Controllers
         // GET: CondicionDeLaCalzadas
         public ActionResult Index()
         {
+            ViewBag.Type = TempData["Type"] != null ? TempData["Type"].ToString() : "";
+            ViewBag.Message = TempData["Message"] != null ? TempData["Message"].ToString() : "";
             return View(db.CONDCALZADA.ToList());
+        }
+
+        public string Verificar(int id)
+        {
+            string mensaje = "";
+            bool exist = db.CARROCERIA.Any(x => x.Id == id);
+            if (exist)
+            {
+                mensaje = "El codigo " + id + " ya esta registrado";
+            }
+            return mensaje;
         }
 
         // GET: CondicionDeLaCalzadas/Details/5
@@ -51,8 +64,22 @@ namespace Cosevi.SIBOAC.Controllers
             if (ModelState.IsValid)
             {
                 db.CONDCALZADA.Add(condicionDeLaCalzada);
-                db.SaveChanges();
-                return RedirectToAction("Index");
+                string mensaje = Verificar(condicionDeLaCalzada.Id);
+                if (mensaje == "")
+                {
+                    db.SaveChanges();
+
+                    TempData["Type"] = "success";
+                    TempData["Message"] = "El registro se realizó correctamente";
+                    return RedirectToAction("Index");
+
+                }
+                else
+                {
+                    ViewBag.Type = "warning";
+                    ViewBag.Message = mensaje;
+                    return View(condicionDeLaCalzada);
+                }
             }
 
             return View(condicionDeLaCalzada);
