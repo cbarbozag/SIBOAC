ea83c03eab150a4561f5c465cdf76d15a3ea7ab9
Carlos Barboza <Carlos Barboza@001-ADC-GREIVIN>
2016-11-25 15:49:59 -0600

    Agregar controlador de la tabla Nacionalidad

---
+++ Cosevi.SIBOAC/Controllers/NacionalidadsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Cosevi.SIBOAC.Models;
+
+namespace Cosevi.SIBOAC.Controllers
+{
+    public class NacionalidadsController : Controller
+    {
+        private PC_HH_AndroidEntities db = new PC_HH_AndroidEntities();
+
+        // GET: Nacionalidads
+        public ActionResult Index()
+        {
+            return View(db.NACIONALIDAD.ToList());
+        }
+
+        // GET: Nacionalidads/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Nacionalidad nacionalidad = db.NACIONALIDAD.Find(id);
+            if (nacionalidad == null)
+            {
+                return HttpNotFound();
+            }
+            return View(nacionalidad);
+        }
+
+        // GET: Nacionalidads/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Nacionalidads/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Descripcion,Estado,FechaDeInicio,FechaDeFin,Prioridad")] Nacionalidad nacionalidad)
+        {
+            if (ModelState.IsValid)
+            {
+                db.NACIONALIDAD.Add(nacionalidad);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(nacionalidad);
+        }
+
+        // GET: Nacionalidads/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Nacionalidad nacionalidad = db.NACIONALIDAD.Find(id);
+            if (nacionalidad == null)
+            {
+                return HttpNotFound();
+            }
+            return View(nacionalidad);
+        }
+
+        // POST: Nacionalidads/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Descripcion,Estado,FechaDeInicio,FechaDeFin,Prioridad")] Nacionalidad nacionalidad)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(nacionalidad).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(nacionalidad);
+        }
+
+        // GET: Nacionalidads/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Nacionalidad nacionalidad = db.NACIONALIDAD.Find(id);
+            if (nacionalidad == null)
+            {
+                return HttpNotFound();
+            }
+            return View(nacionalidad);
+        }
+
+        // POST: Nacionalidads/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            Nacionalidad nacionalidad = db.NACIONALIDAD.Find(id);
+            db.NACIONALIDAD.Remove(nacionalidad);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
