cbe835305e5de53ce8c43e344f7d80ea04f65273
nzamora <nzamora@ADC-LT-DESA01>
2016-11-28 17:28:51 -0600

    actualizar DisXRolPersona

--- Cosevi.SIBOAC/Controllers/DispositivoPorRolPersonasController.cs
+++ Cosevi.SIBOAC/Controllers/DispositivoPorRolPersonasController.cs
@@ -17,13 +17,15 @@ namespace Cosevi.SIBOAC.Controllers
         // GET: DispositivoPorRolPersonas
         public ActionResult Index()
         {
+            ViewBag.Type = TempData["Type"] != null ? TempData["Type"].ToString() : "";
+            ViewBag.Message = TempData["Message"] != null ? TempData["Message"].ToString() : "";
             return View(db.DISPXROLPERSONA.ToList());
         }
 
         // GET: DispositivoPorRolPersonas/Details/5
-        public ActionResult Details(string CodRol, int CodDisp)
+        public ActionResult Details(string CodRol, int? CodDisp)
         {
-            if (CodRol == null)
+            if (CodRol == null|| CodDisp == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
@@ -51,17 +53,32 @@ namespace Cosevi.SIBOAC.Controllers
             if (ModelState.IsValid)
             {
                 db.DISPXROLPERSONA.Add(dispositivoPorRolPersona);
-                db.SaveChanges();
-                return RedirectToAction("Index");
+                string mensaje = Verificar(dispositivoPorRolPersona.DescripcionRolPersona,
+                                            dispositivoPorRolPersona.CodigoDispositivo);
+                if (mensaje == "")
+                {
+                    db.SaveChanges();
+
+                    TempData["Type"] = "success";
+                    TempData["Message"] = "El registro se realizó correctamente";
+                    return RedirectToAction("Index");
+
+                }
+                else
+                {
+                    ViewBag.Type = "warning";
+                    ViewBag.Message = mensaje;
+                    return View(dispositivoPorRolPersona);
+                }
             }
 
             return View(dispositivoPorRolPersona);
         }
 
         // GET: DispositivoPorRolPersonas/Edit/5
-        public ActionResult Edit(string CodRol, int CodDisp)
+        public ActionResult Edit(string CodRol, int? CodDisp)
         {
-            if (CodRol == null)
+            if (CodRol == null|| CodDisp == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
@@ -91,31 +108,8 @@ namespace Cosevi.SIBOAC.Controllers
             return View(dispositivoPorRolPersona);
         }
 
-        // GET: DispositivoPorRolPersonas/Delete/5
-        public ActionResult Delete(string CodRol, int CodDisp)
-        {
-            if (CodRol == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            DispositivoPorRolPersona dispositivoPorRolPersona = db.DISPXROLPERSONA.Find(CodRol, CodDisp);
-            if (dispositivoPorRolPersona == null)
-            {
-                return HttpNotFound();
-            }
-            return View(dispositivoPorRolPersona);
-        }
+     
 
-        // POST: DispositivoPorRolPersonas/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(string CodRol, int CodDisp)
-        {
-            DispositivoPorRolPersona dispositivoPorRolPersona = db.DISPXROLPERSONA.Find(CodRol, CodDisp);
-            db.DISPXROLPERSONA.Remove(dispositivoPorRolPersona);
-            db.SaveChanges();
-            return RedirectToAction("Index");
-        }
 
         protected override void Dispose(bool disposing)
         {
@@ -124,6 +118,21 @@ namespace Cosevi.SIBOAC.Controllers
                 db.Dispose();
             }
             base.Dispose(disposing);
+        }
+
+        public string Verificar(string CodRol, int CodDisp)
+        {
+            string mensaje = "";
+            bool exist = db.DISPXROLPERSONA.Any(x => x.CodigoRolPersona == CodRol
+                                                    && x.CodigoDispositivo == CodDisp);
+            if (exist)
+            {
+                mensaje = "El registro con los siguientes datos ya se encuentra registrados:" +
+                           " código de Rol Persona" + CodRol +
+                           ", código de Dispositivo" + CodDisp;
+
+            }
+            return mensaje;
         }
     }
 }
