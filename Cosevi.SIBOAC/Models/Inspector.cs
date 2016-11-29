fc3f92767b59ca45a9f717c90b5922be16be61c4
Carlos Barboza <Carlos Barboza@001-ADC-GREIVIN>
2016-11-29 12:07:04 -0600

    agregar ID a los mantenimientos

--- Cosevi.SIBOAC/Models/Inspector.cs
+++ Cosevi.SIBOAC/Models/Inspector.cs
@@ -21,31 +21,71 @@ namespace Cosevi.SIBOAC.Models
         [StringLength(4, ErrorMessage = "El código no debe ser mayor a 4 caracteres")]
         public string Id { get; set; }
 
+        [DisplayName("Tipo de identificación")]
+        [Required(ErrorMessage = "El tipo de identificación es obligatorio")]
         public string TipoDeIdentificacion { get; set; }
+
+        [DisplayName("Identificación")]
+        [Required(ErrorMessage = "La identificación es obligatoria")]
         public string Identificacion { get; set; }
 
         [DisplayName("Nombre Completo")]
         [Required(ErrorMessage = "El nombre es obligatorio")]
         [StringLength(50, ErrorMessage = "La descripción no debe ser mayor a 50 caracteres")]
         public string Nombre { get; set; }
+
+        [DisplayName("Primer apellido")]
         public string Apellido1 { get; set; }
+
+        [DisplayName("Segundo apellido")]
         public string Apellido2 { get; set; }
 
         [DisplayName("Ad honorem")]
-        [Required(ErrorMessage = "El campo es obligatorio")]
-        [StringLength(10, ErrorMessage = "El código no debe ser mayor a 10 caracteres")]
+        //[Required(ErrorMessage = "El campo es obligatorio")]
+        //[StringLength(10, ErrorMessage = "El código no debe ser mayor a 10 caracteres")]
         public string Adonoren { get; set; }
 
+        [DisplayName("Fecha de inclusion")]
         public string FechaDeInclusion { get; set; }
+
+        [DisplayName("Fecha de exclusion")]
         public string FechaDeExclusion { get; set; }
+
+        [DisplayName("Documento de inclusion")]
         public string DocumentoDeInclusion { get; set; }
+
+        [DisplayName("Documento de exclusion")]
         public string DocumentoDeExclusion { get; set; }
+
+        [DisplayName("Fecha registro")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public Nullable<System.DateTime> FechaReag { get; set; }
+
+        [DisplayName("Fecha de registro")]
         public string DocumentoReag { get; set; }
+
+        [DisplayName("Codigo de delegación")]
         public string CodigoDeDelegacion { get; set; }
+
+        [DisplayName("Email")]
         public string Email { get; set; }
+
+        [DisplayName("Estado")]
+        [Required(ErrorMessage = "El estado es obligatorio")]
+        [StringLength(1, ErrorMessage = "El estado no debe ser mayor a 1 caracter")]
         public string Estado { get; set; }
+
+        [DisplayName("Fecha de Inicio")]
+        [Required(ErrorMessage = "La fecha de inicio es obligatoria")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public Nullable<System.DateTime> FechaDeInicio { get; set; }
+
+        [DisplayName("Fecha de Fin")]
+        [Required(ErrorMessage = "La fecha de fin es obligatoria")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public Nullable<System.DateTime> FechaDeFin { get; set; }
     }
 }
