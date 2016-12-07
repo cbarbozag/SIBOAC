a5fdfe122109327bb5b02ca90f31dc7edec42449
soporte <soporte@SERVIDOR>
2016-12-07 16:26:40 -0600

    Cambios ReimpresionDeBoletas

---
+++ Cosevi.SIBOAC/Controllers/ReimpresionDeBoletasController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Cosevi.SIBOAC.Controllers
+{
+    public class ReimpresionDeBoletasController : Controller
+    {
+        // GET: ReimpresionDeBoletas
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
