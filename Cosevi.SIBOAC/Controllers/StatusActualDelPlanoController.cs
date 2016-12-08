6edcb1362f5c57fe5a11f6cc093d548e78b51e7f
soporte <soporte@SERVIDOR>
2016-12-08 15:30:30 -0600

    commit

---
+++ Cosevi.SIBOAC/Controllers/StatusActualDelPlanoController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Cosevi.SIBOAC.Controllers
+{
+    public class StatusActualDelPlanoController : Controller
+    {
+        // GET: StatusActualDelPlano
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Cosevi.SIBOAC/Models/StatusActualDelPlano.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Cosevi.SIBOAC.Models
+{
+    public class StatusActualDelPlano
+    {
+        public string CodigoAutoridad { get; set; }
+
+        public string DescripcionAutoridad { get; set; }
+
+        public DateTime FechaAccidente { get; set; }
+
+        public Nullable< DateTime> FechaDescarga  { get; set; }
+
+        public decimal NumeroBoleta { get; set; }
+
+        public string CodigoRol { get; set; }
+
+        public string ClasePlaca { get; set; }
+
+        public string CodigoPlaca { get; set; }
+
+        public string NumeroPlaca { get; set; }
+
+        public Nullable<int> EstadoPlano { get; set; }
+
+        public string DescripcionRol { get; set; }
+
+    }
+}
