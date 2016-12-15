d0f2822173a5318aba7567b5537ee92cb4a05520
Eduardo Oviedo Blanco <eduardo.oviedo@gmail.com>
2016-12-15 07:05:10 -0600

    Reporte 1

--- Cosevi.SIBOAC/Scripts/reportePorUsuario.js
+++ Cosevi.SIBOAC/Scripts/reportePorUsuario.js
@@ -6,7 +6,9 @@
     });
 
     function prepararCampos() {
-        $('.input-daterange').datepicker({});
+        $('.input-daterange').datepicker({
+            startView: 2
+        });
     };
 
     function cargarDatos() {
@@ -17,7 +19,7 @@
             success: function (results) {
                 var list = $('#list');
                 for (var i = 0; i < results.length; i++) {
-                    list.append('<li class="list-group-item">' + results[i].Usuario + ' - ' + results[i].Nombre + '</li>');
+                    list.append('<li data-id=' + results[i].Id + ' class="list-group-item">' + results[i].Usuario + ' - ' + results[i].Nombre + '</li>');
                 };
             },
             error: function (jqXHR, textStatus, errorThrown) {
@@ -32,14 +34,23 @@
     };
 
     function generarReporte() {
+        var idUsuarios = '';
+        var desde = $('#desde').val();
+        var hasta = $('#hasta').val();
+        var usuariosSeleccionados = $('li.active');
+        for (var i = 0; i < usuariosSeleccionados.length; i++) {
+            idUsuarios += 'idUsuarios=' + $(usuariosSeleccionados[i]).data('id') + '&';
+        };
+
+        
+
         $.ajax({
             type: 'GET',
-            url: '/api/SIBOACUsuarios',
-            //data: {},
+            url: '/api/ReportesPorUsuario?' + idUsuarios + 'desde='+ desde +'&hasta='+ hasta,
             success: function (results) {
-                var list = $('#list');
+                var reporte = $('#reporte');                
                 for (var i = 0; i < results.length; i++) {
-                    list.append('<li class="list-group-item">' + results[i].Usuario + ' - ' + results[i].Nombre + '</li>');
+                    reporte.append('<div class="row"><div class="col-md-2">' + results[i].Usuario + '</div><div class="col-md-1">' + results[i].Autoridad + '</div><div class="col-md-1">' + results[i].FechaAccidente + '</div><div class="col-md-1">' + results[i].Serie + '</div><div class="col-md-1">' + results[i].NumeroParte + '</div><div class="col-md-1">' + results[i].Boletas + '</div><div class="col-md-1">' + results[i].FechaDescarga + '</div><div class="col-md-1">' + results[i].ClasePlaca + '</div><div class="col-md-1">' + results[i].CodigoPlaca + '</div><div class="col-md-1">' + results[i].NumeroPlaca + '</div><div class="col-md-1">' + results[i].EstadoPlano + '</div></div>');
                 };
             },
             error: function (jqXHR, textStatus, errorThrown) {
