1d9afaa507145548c03f697e0a62f4ffcb945d15
Carlos Barboza <Carlos Barboza@001-ADC-GREIVIN>
2016-12-19 14:34:06 -0600

    agregar reporte de parte oficial

--- Cosevi.SIBOAC/App_Start/BundleConfig.cs
+++ Cosevi.SIBOAC/App_Start/BundleConfig.cs
@@ -39,6 +39,10 @@ namespace Cosevi.SIBOAC
             bundles.Add(new ScriptBundle("~/bundles/ReportePorEstadoActualDelPlano").Include(
                      "~/Scripts/reportePorEstadoActualDelPlano.js"));
 
+            // Report: ReportesPorConsultaParteOficial bundles
+            bundles.Add(new ScriptBundle("~/bundles/ReportePorConsultaParteOficial").Include(
+                     "~/Scripts/ReportesPorConsultaParteOficial.js"));
+
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/bootstrap-datepicker.min.css",
---
+++ Cosevi.SIBOAC/Scripts/ReportesPorConsultaParteOficial.js
@@ -0,0 +1,30 @@
+﻿(function () {
+    $(document).ready(function () {
+        prepararCampos();
+        cargarDatos();
+        prepararEventos();
+    });
+
+    function prepararCampos() {
+        $(":input[type='radio']").on("change", function () {
+            $('div[id^=opc-]').hide();
+            if ($(this).prop("checked")) {
+                var checkedRadio = '#opc-' + $(this).val();
+                $(checkedRadio).show();
+            }
+        });
+    };
+
+    function cargarDatos() {
+   
+    };
+
+    function prepararEventos() {
+        $('#generar').click(generarReporte);
+    };
+
+    function generarReporte() {
+       
+    };
+
+})();
