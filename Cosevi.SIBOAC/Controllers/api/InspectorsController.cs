4353e7b30a841da20da8467ce933ef4e8ed6fd7d
soporte <soporte@SERVIDOR>
2016-12-23 09:50:11 -0600

    Cambios en reportepordescargadeboletas

---
+++ Cosevi.SIBOAC/Controllers/api/InspectorsController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Cosevi.SIBOAC.Models;
+
+namespace Cosevi.SIBOAC.Controllers.api
+{
+    public class InspectorsController : ApiController
+    {
+        private PC_HH_AndroidEntities db = new PC_HH_AndroidEntities();
+
+        // GET: api/Inspectors
+        public IQueryable<Inspector> GetINSPECTOR()
+        {
+            return db.INSPECTOR;
+        }
+
+        // GET: api/Inspectors/5
+        [ResponseType(typeof(Inspector))]
+        public IHttpActionResult GetInspector(string id)
+        {
+            Inspector inspector = db.INSPECTOR.Find(id);
+            if (inspector == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(inspector);
+        }
+
+        // PUT: api/Inspectors/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutInspector(string id, Inspector inspector)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != inspector.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(inspector).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!InspectorExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Inspectors
+        [ResponseType(typeof(Inspector))]
+        public IHttpActionResult PostInspector(Inspector inspector)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.INSPECTOR.Add(inspector);
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateException)
+            {
+                if (InspectorExists(inspector.Id))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtRoute("DefaultApi", new { id = inspector.Id }, inspector);
+        }
+
+        // DELETE: api/Inspectors/5
+        [ResponseType(typeof(Inspector))]
+        public IHttpActionResult DeleteInspector(string id)
+        {
+            Inspector inspector = db.INSPECTOR.Find(id);
+            if (inspector == null)
+            {
+                return NotFound();
+            }
+
+            db.INSPECTOR.Remove(inspector);
+            db.SaveChanges();
+
+            return Ok(inspector);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool InspectorExists(string id)
+        {
+            return db.INSPECTOR.Count(e => e.Id == id) > 0;
+        }
+    }
+}
---
+++ Cosevi.SIBOAC/Scripts/ReportePorConsultaImpresionDeBoletas.js
@@ -0,0 +1,50 @@
+﻿(function () {
+    $(document).ready(function () {
+        prepararCampos();
+        cargarDatosDelegacion();
+        cargarDatosInspector();
+        prepararEventos();
+    });
+
+    function prepararCampos() {
+        $('.input-daterange').datepicker({
+            startView: 2
+        });
+    };
+
+
+    function cargarDatosDelegacion() {
+        $.ajax({
+            type: 'GET',
+            url: '/api/Delegacions',
+            //data: {},
+            success: function (results) {
+                var listD = $('#listaDelegaciones');
+                for (var i = 0; i < results.length; i++) {
+                    listD.append('<li data-id=' + results[i].Id + ' class="list-group-item">' + results[i].Descripcion + '</li>');
+                };
+            },
+            error: function (jqXHR, textStatus, errorThrown) {
+                console.log(jqXHR);
+            }            
+        });
+    };
+
+
+
+    function cargarDatosInspector() {
+        $.ajax({
+            type: 'GET',
+            url: '/api/Inspectors',
+            //data: {},
+            success: function (results) {
+                var listA = $('#listaInspectores');
+                for (var i = 0; i < results.length; i++) {
+                    listA.append('<li data-id=' + results[i].Id + ' class="list-group-item">' + results[i].Descripcion + '</li>');
+                };
+            },
+            error: function (jqXHR, textStatus, errorThrown) {
+                console.log(jqXHR);
+            }
+        });
+    };
