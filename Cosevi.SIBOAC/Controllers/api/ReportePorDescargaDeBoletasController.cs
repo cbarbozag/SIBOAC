ecaf631df0a8cae745b74ae0845865c9e831d14a
soporte <soporte@SERVIDOR>
2016-12-28 07:59:47 -0600

    cambios en reportes

--- Cosevi.SIBOAC/Controllers/api/ReportePorDescargaDeBoletasController.cs
+++ Cosevi.SIBOAC/Controllers/api/ReportePorDescargaDeBoletasController.cs
@@ -23,12 +23,6 @@ namespace Cosevi.SIBOAC.Controllers.api
             if (idRadio == 1)
             {
                 var reportes =
-                //(from bo in db.BOLETA
-                // join po in db.PARTEOFICIAL on new { NumeroParte = bo.numeroparte } equals new { NumeroParte = po.NumeroParte }
-                // join del in db.DELEGACION on new { Id = bo.codigo_delegacion } equals new { Id = (string)del.Id }
-                // join au in db.AUTORIDAD on new { Id = bo.codigo_autoridad_registra } equals new { Id = (string)au.Id }
-                // where
-                // po.fecha_descarga >= desde && po.fecha_descarga <= hasta
 
                 (from bo in db.BOLETA
                  join per in db.PERSONA on bo.identificacion equals per.identificacion
