efa4cc69e21d2861f3fbb5697ad71f271af38841
Greivin Alvarado <galvarado@adcmovil.com>
2017-01-05 10:12:07 -0600

    modificar modelo usuarios

--- Cosevi.SIBOAC/Models/PC_HH_AndroidModel.Designer.cs
+++ Cosevi.SIBOAC/Models/PC_HH_AndroidModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\eduar\documents\visual studio 2015\Projects\Cosevi.SIBOAC\Cosevi.SIBOAC\Models\PC_HH_AndroidModel.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\Carlos Barboza\Source\Repos\SIBOAC\Cosevi.SIBOAC\Models\PC_HH_AndroidModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
--- Cosevi.SIBOAC/Models/SIBOACSecurityModel.Designer.cs
+++ Cosevi.SIBOAC/Models/SIBOACSecurityModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Respaldo Cosevi SIBOAC\Cosevi.SIBOAC\Models\SIBOACSecurityModel.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\Carlos Barboza\Source\Repos\SIBOAC\Cosevi.SIBOAC\Models\SIBOACSecurityModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
