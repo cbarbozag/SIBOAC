2782edac0b0946ef1c182ba152996ac1ab15d9b0
Pablo Rodriguez Apú <prodriguez@gpilatam.com>
2017-01-06 01:06:48 -0600

    Primera modificacion para el manejo de seguridad por roles de forma dinamica

---
+++ Cosevi.SIBOAC/Security/Utilities.cs
@@ -0,0 +1,24 @@
+﻿using Cosevi.SIBOAC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Cosevi.SIBOAC.Security
+{
+    public static class Utilities
+    {
+        public static List<SIBOACMenuOpciones> GetMenuOptions(string userName)
+        {
+            using (SIBOACSecurityEntities sdb = new SIBOACSecurityEntities())
+            {
+                var user = sdb.SIBOACUsuarios.Where(a => a.Usuario.Equals(userName)).FirstOrDefault();
+
+                int rolID = user.SIBOACRolesDeUsuarios.FirstOrDefault().IdRol;
+
+                var menuOptions = sdb.SIBOACMenuOpciones.Where(m => m.SIBOACRoles.Any(r => r.Id == rolID) && m.Estado).ToList();
+                return menuOptions;
+            }
+        }
+    }
+}
