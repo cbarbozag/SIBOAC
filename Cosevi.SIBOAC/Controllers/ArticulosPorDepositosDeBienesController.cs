cb2feb9088080cfcb77928900efe0223fb2ba456
nzamora <nzamora@ADC-LT-DESA01>
2017-01-09 10:50:07 -0600

    Actualizacion de combos dependientes

--- Cosevi.SIBOAC/Controllers/ArticulosPorDepositosDeBienesController.cs
+++ Cosevi.SIBOAC/Controllers/ArticulosPorDepositosDeBienesController.cs
@@ -214,6 +214,22 @@ namespace Cosevi.SIBOAC.Controllers
                 {
                     ViewBag.Type = "warning";
                     ViewBag.Message = mensaje;
+
+                    ViewBag.ComboArticulos = null;
+                    ViewBag.ComboArticulos = new SelectList((from o in db.CATARTICULO
+                                                             where o.Estado == "A"
+                                                             select new { o.Id }).ToList().Distinct(), "Id", "Id", articulosPorDepositosDeBienes.CodigoArticulo);
+
+                    ViewBag.ComboDepositosBienes = new SelectList((from o in db.DEPOSITOBIENES
+                                                                   select new { o.Id,o.Descripcion }).ToList().Distinct(), "Id", "Descripcion", articulosPorDepositosDeBienes.CodigoDepositosBienes);
+
+                    ViewBag.ComboOpcionFormulario = new SelectList((from o in db.OPCIONFORMULARIO
+                                                                    select new { o.Id, o.Descripcion }).ToList().Distinct(), "Id", "Descripcion", articulosPorDepositosDeBienes.CodigoOpcionFormulario);
+
+
+                    ViewData["Conducta"] = articulosPorDepositosDeBienes.Conducta;
+                    ViewData["FechaDeInicio"] = articulosPorDepositosDeBienes.FechaDeInicio.ToString("dd-MM-yyyy");
+                    ViewData["FechaDeFin"] = articulosPorDepositosDeBienes.FechaDeFin.ToString("dd-MM-yyyy");
                     return View(articulosPorDepositosDeBienes);
                 }
             }
@@ -369,7 +385,7 @@ namespace Cosevi.SIBOAC.Controllers
             return RedirectToAction("Index");
         }
 
-        [HttpPost]
+    
         public JsonResult ObtenerConducta(string CodigoArticulo)
         {
             var listaconducta =
@@ -388,7 +404,7 @@ namespace Cosevi.SIBOAC.Controllers
                 
             return Json(listaconducta,JsonRequestBehavior.AllowGet);
         }
-        [HttpPost]
+   
         public JsonResult ObtenerFechaInicio(string CodigoArticulo,string Conducta)
         {
             var listaconducta =
@@ -401,26 +417,32 @@ namespace Cosevi.SIBOAC.Controllers
                   }).ToList().Distinct()
             .Select(x => new ClaseFechaInicio
             {
-                Id = x.fecha,
+                Id = DateTime.Parse(x.fecha).ToString("dd-MM-yyyy"),
                 Nombre = DateTime.Parse(x.fecha).ToString("dd-MM-yyyy")
             }).Distinct();
 
             return Json(listaconducta, JsonRequestBehavior.AllowGet);
         }
 
-        [HttpPost]
+      
         public JsonResult ObtenerFechaFinal(string CodigoArticulo, string Conducta,string FechaDeInicio)
         {
+            DateTime fechaDeInicio = DateTime.Now;
+            if (FechaDeInicio != "" && FechaDeInicio != null)
+            {
+                fechaDeInicio = DateTime.Parse(FechaDeInicio);
+                // FechaDeInicio = fechaDeInicio.ToString();
+            }
             var listaconducta =
                  (from o in db.CATARTICULO
-                  where o.Id == CodigoArticulo && o.Conducta == Conducta && o.Estado == "A" && o.FechaDeInicio.ToString() == FechaDeInicio
+                  where o.Id == CodigoArticulo && o.Conducta == Conducta && o.Estado == "A" && o.FechaDeInicio == fechaDeInicio
                   select new
                   {
                       fecha = o.FechaDeFin.ToString()
                   }).ToList().Distinct()
             .Select(x => new ClaseFechaFinal
             {
-                Id = x.fecha,
+                Id = DateTime.Parse(x.fecha).ToString("dd-MM-yyyy"),
                 Nombre = DateTime.Parse(x.fecha).ToString("dd-MM-yyyy")
             }).Distinct();
 
