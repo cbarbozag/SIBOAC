48955ca24efbb404448d780605de073f9e9043ee
Pablo Rodriguez Apú <prodriguez@gpilatam.com>
2017-02-01 02:40:39 -0600

    Se agrega funcionalidad de imprimir en los reportes, para evitar la limitacion a IE

--- Cosevi.SIBOAC/Reports/ViewerPage.aspx
+++ Cosevi.SIBOAC/Reports/ViewerPage.aspx
@@ -8,12 +8,17 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head runat="server">
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <title></title>
+    <title></title>    
 </head>
 <body style="width:auto">
     <form id="form1" runat="server">
         <asp:ScriptManager ID="ScriptManager1" runat="server"></asp:ScriptManager>
-        <rsweb:ReportViewer ID="ReportViewer1" Height="800px" Width="99%" runat="server" ZoomMode="FullPage"></rsweb:ReportViewer>
+        <asp:Button ID="btnPrint" runat="server" OnClick="btnPrint_Click" Text="Imprimir" Visible="false" />
+&nbsp;<rsweb:ReportViewer ID="ReportViewer1" Height="800px" Width="99%" runat="server" ZoomMode="FullPage"></rsweb:ReportViewer>
+
+        <iframe id="frmPrint" name="IframeName" width="500" 
+  height="200" runat="server" 
+  style="display: none" runat="server"></iframe>
     </form>
 </body>
 </html>
--- Cosevi.SIBOAC/Reports/ViewerPage.aspx.designer.cs
+++ Cosevi.SIBOAC/Reports/ViewerPage.aspx.designer.cs
@@ -31,6 +31,15 @@ namespace Cosevi.SIBOAC.Reports {
         protected global::System.Web.UI.ScriptManager ScriptManager1;
         
         /// <summary>
+        /// btnPrint control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnPrint;
+        
+        /// <summary>
         /// ReportViewer1 control.
         /// </summary>
         /// <remarks>
@@ -38,5 +47,14 @@ namespace Cosevi.SIBOAC.Reports {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::Microsoft.Reporting.WebForms.ReportViewer ReportViewer1;
+        
+        /// <summary>
+        /// frmPrint control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlIframe frmPrint;
     }
 }
