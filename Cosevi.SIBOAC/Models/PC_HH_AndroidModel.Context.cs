903223e624a66b091dd95644f67a8b15343628f9
Greivin Alvarado <galvarado@adcmovil.com>
2018-03-23 12:07:39 -0600

    agregar tabla testigo por boleta

--- Cosevi.SIBOAC/Models/PC_HH_AndroidModel.Context.cs
+++ Cosevi.SIBOAC/Models/PC_HH_AndroidModel.Context.cs
@@ -229,6 +229,8 @@ public partial class PC_HH_AndroidEntities : DbContext
 
     public virtual DbSet<OtrosAdjuntos> OtrosAdjuntos { get; set; }
 
+    public virtual DbSet<TESTIGOXBOLETA> TESTIGOXBOLETA { get; set; }
+
 
     public virtual ObjectResult<BitacoraSIBOAC> GetBitacoraData(Nullable<System.DateTime> fechaInicio, Nullable<System.DateTime> fechaFin, string nombreTabla, string usuario, string operacion)
     {
